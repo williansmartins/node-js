5da329e53523ff62301e3406e31785afe814c25c
Willians Martins <professorwillians@gmail.com>
2019-04-22 17:29:40 -0300

    Create reduce-average.js

---
+++ reduce-average.js
@@ -0,0 +1,10 @@
+const euros = [29.76, 41.85, 46.5];
+const average = euros.reduce((total, amount, index, array) => {
+  total += amount;
+  if( index === array.length-1) { 
+    return total/array.length;
+  }else { 
+    return total;
+  }
+});
+average // 39.37
